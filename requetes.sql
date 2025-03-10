a277ebeb8b93a4750eb970d5906f65dce13fe8dd
alexisnisol <alexis.nisol@etu.univ-orleans.fr>
2025-03-10 17:18:42 +0100

    Ajout Requête 4

--- requetes.sql
+++ requetes.sql
@@ -4,3 +4,9 @@ select p.libelle_piece, quantite from PIECE p
 join COMPOSER on ref_piece = ref_piece_composante
 join PIECE p_composee on ref_piece_composee = p_composee.ref_piece
 where p_composee.libelle_piece = 'Panneau de fuselage';
+
+-- 4 Comptez combien de pièces composées existent dans la base.
+
+select count(*) AS NB_PIECES_COMPOSEES from PIECE p
+where exists (select * from COMPOSER c where c.ref_piece_composee = p.ref_piece);
+
